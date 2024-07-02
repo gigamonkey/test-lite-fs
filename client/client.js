692dd59f6edaf28edb3e98599bfb1fad41fdb0e7
Peter Seibel <peter@gigamonkeys.com>
2024-07-02 10:59:08 -0700

    Client starting to be a bit real: cookies, loop, capturing failures and timing.

--- client/client.js
+++ client/client.js
@@ -1,9 +1,32 @@
 import { argv } from 'process';
+import { CookieJar } from 'tough-cookie';
+import fetchCookie from 'fetch-cookie';
+import now from 'performance-now';
 
-const [ url ] = argv.slice(2)
+const cookies = new CookieJar();
+const fetchWithCookies = fetchCookie(fetch, cookies);
+
+const requests = [];
+
+const timedFetch = async (...args) => {
+  const start = now();
+  let ok = true;
+  let status = undefined;
+  try {
+    const r = await fetchWithCookies(...args);
+    status = r.status;
+    return r;
+  } catch (e) {
+    ok = false;
+    return undefined;
+  } finally {
+    const ms = now() - start;
+    requests.push({ ms, ok, status });
+  }
+};
 
 const addStuff = async (tag, number) => {
-  return fetch(url, {
+  return timedFetch(url, {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
@@ -13,12 +36,47 @@ const addStuff = async (tag, number) => {
 };
 
 const getMe = async (tag) => {
-  return fetch(`${url}/me?tag=${tag}`);
+  return fetchWithCookies(`${url}/me?tag=${tag}`);
 }
 
-//const r = await fetch(url);
+const dumpCookies = () => {
+  cookies.getCookies(url, { allPaths: true }, (err, cookies) => {
+    if (err) {
+      console.error('Error getting cookies:', err);
+    } else {
+      console.log('Cookies stored in jar:', cookies);
+    }
+  });
+};
 
-//console.log(await r.json());
 
-console.log(await addStuff('a', 1).then(r => r.json()));
-console.log(await getMe('a').then(r => r.json()));
+let [ url, tag, num, delay ] = argv.slice(2);
+
+num = Number(num);
+delay = Number(delay);
+
+const start = await getMe(tag).then(r => r.json());
+
+console.log(start);
+
+const offset = start.max + 1;
+
+const busywait = (ms) => {
+  const start = now();
+  while (now() - start < ms);
+}
+
+for (let i = 0; i < num; i++) {
+  const r = await addStuff(tag, i + offset);
+  if (delay > 0) busywait(delay);
+}
+
+try {
+  console.log(await getMe('a').then(r => r?.json()));
+} catch {
+  console.log('Problem at end');
+}
+
+//dumpCookies();
+
+requests.forEach(r => console.log(JSON.stringify(r)));
